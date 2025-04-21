31df5f5b1649b1be2c4c0169baf9996557d4c16a
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-04-21 17:11:52 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1384 ms, Memory: 78268 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+num_list = []
+
+for _ in range(n):
+    num_list.append(int(input()))
+
+num_list.sort()
+
+for i in range(n):
+    print(num_list[i])
