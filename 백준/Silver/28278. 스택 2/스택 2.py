3994ee361fd8e3f4daef9fef79c04c1ba3198248
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-04-25 16:42:38 +0900

    [Silver IV] Title: 스택 2, Time: 864 ms, Memory: 72404 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    a = input().split()
+    if a[0] == '1':
+        stack.append(int(a[1]))
+    elif a[0] == "2":
+        if len(stack) > 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif a[0] == "3":
+        print(len(stack))
+    elif a[0] == "4":
+        if len(stack) > 0:
+            print(0)
+        else:
+            print(1)
+    elif a[0] == "5":
+        if len(stack) > 0:
+            print(stack[len(stack) - 1])
+        else:
+            print(-1)
