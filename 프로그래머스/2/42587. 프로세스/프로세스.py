c9c7df36b74b793bd5369916d4d7a5f2b30f68c6
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-10-03 12:29:52 +0900

    [level 2] Title: 프로세스, Time: 0.91 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(priorities, location):
+    new_priorities = deque()
+    count = 0
+    answer = 0
+    
+    for i in range(len(priorities)):
+        new_priorities.append((i, priorities[i]))
+        
+    while new_priorities:
+        b = new_priorities.popleft()
+        if b[1] >= max(priorities):
+            count += 1
+            priorities.remove(b[1])
+            if b[0] == location:
+                return count
+        else:
+            new_priorities.append(b)
