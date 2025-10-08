0161bd351cf1f2c740baefd4cf9ba10d65bf1fb9
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-10-08 23:10:48 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def dfs(word, target, words, visited, word_len, answer):
+    if word == target:
+        answer_list.append(answer)
+        return
+    for i in range(len(words)):
+        count = 0
+        if not visited[i]:
+            for x in range(word_len):
+                if word[x] == words[i][x]:
+                    count += 1
+            if count == word_len - 1:
+                visited[i] = True
+                dfs(words[i], target, words, visited, word_len, answer + 1)
+                visited[i] = False
+                       
+    
+def solution(begin, target, words):
+    global answer_list
+    answer_list = []
+    if not target in words:
+        return 0
+    n = len(words)
+    word_len = len(words[0])
+    visited = [0] * n
+    dfs(begin, target, words, visited, word_len, 0)
+    return min(answer_list)
+        
+        
+        
+        
+    
