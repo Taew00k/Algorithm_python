a6d43f407887a9c00a990344f7f91158cd0aa1f7
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-10-23 15:08:36 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -0,0 +1,42 @@
+n = int(input())
+switch_list = list(map(int, input().split()))
+p = int(input())
+
+for _ in range(p):
+    s,idx = map(int, input().split())
+    if s == 1:
+        for i in range(idx-1, n, idx):
+            if switch_list[i] == 0:
+                switch_list[i] = 1
+            else:
+                switch_list[i] = 0
+    elif s == 2:
+        if switch_list[idx-1] == 0:
+                switch_list[idx-1] = 1
+        else:
+            switch_list[idx-1] = 0
+        left_switch = switch_list[:idx-1]
+        right_switch = switch_list[idx:]
+        count = 1
+        while True:
+            if not left_switch:
+                break
+            if not right_switch:
+                break
+            if not left_switch[-1] == right_switch[0]:
+                break
+            else:
+                if left_switch[-1] == 0:
+                    switch_list[idx-1-count] = 1
+                    switch_list[idx-1+count] = 1
+                else:
+                    switch_list[idx-1-count] = 0
+                    switch_list[idx-1+count] = 0
+            count += 1
+            left_switch.pop()
+            right_switch.pop(0)
+    
+for i in range(n):
+    print(switch_list[i], end=" ")
+    if (i + 1) % 20 == 0:
+        print()
