4e46ea087a839a614e015918edece58df3a04905
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-11-19 23:01:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.93 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,15 @@
 def solution(participant, completion):
-    member = {}
-    for i in range(len(participant)):
-        if participant[i] in member:
-            member[participant[i]] += 1
+    dict = {}
+    answer = []
+    for p in participant:
+        if p in dict:
+            dict[p] += 1
         else:
-            member[participant[i]] = 1
-    for j in range(len(completion)):
-        member[completion[j]] -= 1
-    for k,v in member.items():
-        if v == 1:
+            dict[p] = 1
+    for c in completion:
+        if c in dict:
+            dict[c] -= 1
+    for k,v in dict.items():
+        if not v == 0:
             return k
-        
         
