9497ddb401d79e66ceb5175e9e7df423c2f2e7fb
Taewook <127061738+Taew00k@users.noreply.github.com>
2025-11-20 15:30:57 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,10 @@
+from collections import defaultdict
 def solution(clothes):
-    clothes_type = {}
-    answer = 1
-    for i in clothes:
-        if i[1] not in clothes_type:
-            clothes_type[i[1]] = 2
-        else:
-            clothes_type[i[1]] += 1
-    for j in clothes_type.values() :
-        answer *= j
-    return answer-1
+    sum = 1
+    blanket = defaultdict(list)
+    for c in clothes:
+        blanket[c[1]].append(c[0])
+    for k,v in blanket.items():
+        sum *= (len(blanket[k]) + 1)
+    return sum - 1
+        
